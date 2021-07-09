e2b4a93868706c6599e62455406b274cfb5b663d
NAKAZATO Hajime <hajime@kaoriha.org>
2021-07-09 16:38:53 +0900

    version 0.0.3

--- tests/test_kle_scraper.py
+++ tests/test_kle_scraper.py
@@ -4,12 +4,12 @@ import os
 from PIL import Image, ImageChops
 
 class TestKleScraper(unittest.TestCase):
-    def compare_generated_oracle(self, test_filename, generated_filename, oracle_filename):
+    def compare_generated_oracle(self, test_file, generated_file, oracle_file):
         from kle_scraper import scrape
         with tempfile.TemporaryDirectory() as tmp:
-            scrape(os.path.join('test_data', test_filename), tmp)
-            generated = Image.open(os.path.join(tmp, generated_filename))  # big ass enter
-            oracle = Image.open(os.path.join('test_data', oracle_filename))
+            scrape(os.path.join('test_data', test_file), tmp)
+            generated = Image.open(os.path.join(tmp, generated_file))
+            oracle = Image.open(os.path.join('test_data', oracle_file))
             diff = ImageChops.difference(generated, oracle)
             self.assertFalse(diff.getbbox(), 'Scraped image is different from the oracle.')
 
