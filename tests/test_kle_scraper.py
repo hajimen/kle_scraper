a715a0485581e32f6ebae8c60875654afc46716d
NAKAZATO Hajime <hajime@kaoriha.org>
2025-01-18 12:36:20 +0900

    better oracle matching

--- tests/test_kle_scraper.py
+++ tests/test_kle_scraper.py
@@ -3,24 +3,37 @@ faulthandler.enable()
 import unittest
 import tempfile
 import os
+from pathlib import Path
 from PIL import Image, ImageChops
 
 
 class TestKleScraper(unittest.TestCase):
-    def compare_generated_oracle(self, test_file, generated_file, oracle_file):
+    def compare_generated_oracle(self, test_file: str, generated_file: str, oracle_file: str):
         from kle_scraper import scrape
+
+        test_data_dir = Path('test_data')
+
         with tempfile.TemporaryDirectory() as tmp:
-            scrape(os.path.join('test_data', test_file), tmp)
-            generated = Image.open(os.path.join(tmp, generated_file))
-            oracle_path = os.path.join('test_data', oracle_file)
-            oracle = Image.open(oracle_path)
-            diff = ImageChops.difference(generated, oracle)
-            if diff.getbbox():
-                test_failed_img_dir = 'test_failed_img'
-                os.makedirs(test_failed_img_dir, exist_ok=True)
-                generated_path = os.path.join(test_failed_img_dir, oracle_file)
-                generated.save(generated_path)
-                self.assertFalse(True, f'Scraped image is different from the oracle. Compare {generated_path} with {oracle_path} .')
+            tmp_dir = Path(tmp)
+            scrape(test_data_dir / test_file, tmp_dir)
+            generated = Image.open(tmp_dir / generated_file)
+
+            for oracle_dir in test_data_dir.iterdir():
+                if not oracle_dir.name.startswith('oracle-'):
+                    continue
+                oracle = Image.open(oracle_dir / oracle_file)
+                diff = ImageChops.difference(generated, oracle)
+                bbox = diff.getbbox()
+                if not bbox:
+                    # same with oracle
+                    return
+
+            # same oracle not found
+            test_failed_img_dir = Path('test_failed_img')
+            os.makedirs(test_failed_img_dir, exist_ok=True)
+            generated_path = test_failed_img_dir / oracle_file
+            generated.save(generated_path)
+            self.assertFalse(True, f'Scraped image is different from any oracles. See {generated_path} .')
 
     def test_bigass(self):
         self.compare_generated_oracle('big-ass.json', '51.png', 'big-ass.png')
